3451c6af241b6cae57283291ae0c2967f4f237a7
alheym <alheym.mw@gmail.com>
2023-03-21 10:34:14 +0300

    containers

---
+++ src/containers/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Todos from '../components/Todos'
+
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="app">
+            <Todos />
+        </div>
+    );
+}
